733be2364fc51ed7258c7eda05d9a0c46be8a49a
Steven Zhou <stevenz9@uci.edu>
2023-08-01 18:56:51 -0700

    added nodemailer

---
+++ server.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const express = require("express");
+const OAuth2 = google.auth.OAuth2;
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
+
+const sendEmail = async (recipientEmail) => {
+  let emailTransporter = await createTransporter();
+  await emailTransporter.sendMail({
+    from: process.env.EMAIL,
+    to: recipientEmail,
+    subject: "Testing NodeMailer",
+    text: "You smell... like the smelly smell smell",
+  });
+};
+
+app.get("/", (req, res) => {
+  sendEmail()
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.post("/send_email", (req, res) => {
+  sendEmail(req.body.userEmail)
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
